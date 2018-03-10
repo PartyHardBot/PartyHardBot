19576fe88c9c1ac335c1cebabf4b28d466eda014
Javojav <javipelema@gmail.com>
2018-03-10 14:19:21 +0100

    Better Code (again)

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const bot = new TelegramBot(process.env.API_KEY, {
   polling: true
 });
 
-let linktest = /^(https?:\/\/)?(www.)?youtu((.be)|be.com?)\/(watch\?v=)?\S{11}$/;
+const messages = require('./messages.js');
 
 const firebase = require('firebase');
 firebase.initializeApp({
@@ -19,77 +19,73 @@ firebase.initializeApp({
 const database = firebase.database();
 const ref = database.ref('songs');
 
-var Names = [];
-var Ids = [];
+let Songs = [];
 
 ref.on('value', data => {
-  getdata();
+  Songs = [];
+  for (i of Object.keys(data.val())) {
+    Songs.push({
+      name: data.val()[i].name,
+      id: data.val()[i].id
+    });
+  }
 });
 
-function getdata() {
-  ref.on('value', data => {
-    Names = [];
-    Ids = [];
-    let songs = data.val();
-    let keys = Object.keys(songs);
-    for (let i = 0; i < keys.length; i++) {
-      let k = keys[i];
-      Names.push(songs[k].name);
-      Ids.push(songs[k].id);
-    }
-  });
-}
+bot.onText(/(\/start|\/help)/, msg => bot.sendMessage(msg.chat.id, messages.start));
 
-bot.onText(/(\/start|\/help)/, (msg) => {
-  bot.sendMessage(msg.chat.id, "Hello,\nthis is a bot that sends party hard songs when you type /partyhard.\nIf you want to send a song write /newsong [Name] [Youtube link]");
-  getdata();
-});
-
-bot.onText(/\/partyhard/, (msg) => {
-  getdata();
-  let video = Math.floor((Math.random() * Ids.length));
-  bot.sendMessage(msg.chat.id, Names[video]+"\nhttps://youtu.be/"+Ids[video]);
-});
+bot.onText(/\/partyhard/, msg => bot.sendMessage(msg.chat.id, messages.randomsong(Songs)));
 
 bot.onText(/(\/newsong@InfoPartyHardBot (.+)|\/newsong (.+))/, (msg, match) => {
-  getdata();
   let args = match[1].split(" ");
+
   let link = args[args.length-1];
   let id = link.substring(link.length-11,link.length);
+
   let name = args.slice(1, args.length-1).join(" ");
-  let exists = false;
-  let youtube = linktest.test(link);
-  if (!linktest.test(link)) bot.sendMessage(msg.chat.id, "Please use a youtube link");
-  for (let i = 0; i < Ids.length; i++) {
-    if (id == Ids[i] || name == Names[i]) {
-      exists = true;
-      bot.sendMessage(msg.chat.id, "Another user sent that song");
-    }
-  }
-  if (!exists && youtube) {
+
+  if (!songexists(name, id, msg.chat.id) && youtubelink(link, msg.chat.id)) {
     ref.push(data = {
       name,
       id
     });
-    bot.sendMessage(msg.chat.id, "Your song was saved");
+    bot.sendMessage(msg.chat.id, messages.savedsong);
   }
 });
 
+const songexists = (name, id, msgid) => {
+  for (i of Songs) {
+    if (name == i.name || id == i.id) {
+      bot.sendMessage(msgid, messages.exists);
+      return true;
+    }
+  }
+  return false;
+}
+
+const youtubelink = (link, msgid) => {
+  const linktest = /^(https?:\/\/)?(www.)?youtu((.be)|be.com?)\/(watch\?v=)?\S{11}$/;
+  if (!linktest.test(link)) {
+    bot.sendMessage(msgid, messages.notyoutube);
+    return false;
+  }
+  else return true;
+}
+
 bot.on("inline_query", msg => {
-  getdata();
   let results = [];
-  for (let i = 0; i < Names.length; i++) {
-    for (let j = 0; j < Names[i].length; j++) {
+  const searchfor = msg.query.toLowerCase();
+  for (song of Songs) {
+    for (let j = 0; j < song.name.length; j++) {
       const searchfor = msg.query.toLowerCase();
-      if (Names[i].substring(j, searchfor.length+j).toLowerCase() == searchfor) {
+      if (song.name.substring(j, searchfor.length+j).toLowerCase() == searchfor) {
         results.push({
           "type": "article",
-          "id": msg.id + i,
-          "title": Names[i],
+          "id": msg.id + Songs.indexOf(song),
+          "title": song.name,
           "input_message_content": {
-            "message_text": Names[i] + "\nhttps://youtu.be/" + Ids[i]
+            "message_text": messages.inline(song.name, song.id)
           },
-          "thumb_url": "https://img.youtube.com/vi/"+Ids[i]+"/0.jpg"
+          "thumb_url": "https://img.youtube.com/vi/"+song.id+"/0.jpg"
         });
         break;
       }
---
+++ messages.js
@@ -0,0 +1,15 @@
+const randomsong = (songs) => {
+  let random = Math.floor((Math.random() * songs.length));
+  return songs[random].name+"\nhttps://youtu.be/"+songs[random].id;
+};
+
+const inline = (name, id) => name + "\nhttps://youtu.be/" + id;
+
+module.exports = {
+  start: "Hi,\nI'm a Bot made in node.js that sends songs stored in partyhard.javojav.me, when you write /partyhard.\n If you want to save a song write /newsong [Name of the song] [Youtube link of that song]",
+  randomsong: randomsong,
+  notyoutube: "Please use a valid youtube link",
+  exists: "Another user sent that song",
+  savedsong: "Your song was saved",
+  inline: inline
+}
